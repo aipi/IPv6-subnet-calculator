77affcda790db522a217a3b3461e2960e672afaf
aipi <tesouraet@gmail.com>
2017-04-22 17:25:40 -0300

    hexadecimal converter to work with matrix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #IPv6 subnets finder
 from IPv6_divider import IPv6_divider
-from IPv6_checker import *
+from hexadecimal_converter import *
 from IPv6_validation import *
 
 if __name__ == '__main__': 
@@ -10,13 +10,11 @@ if __name__ == '__main__':
 	divider_result = IPv6_divider(ipv6_to_convert)
 	if(type(divider_result) is list):
 		ipv6_to_convert = divider_result[0]
-		range_of_subnets = divider_result[1]
-		print(divider_result)		
+		range_of_subnets = divider_result[1]	
 	else:
 		ipv6_to_convert = divider_result
-		print(divider_result)
 	
-	IPv6_validation(ipv6_to_convert)
+	Hex_converter(IPv6_validation(ipv6_to_convert))
 
 	#ipv6_to_convert = IPv6_validation(ipv6_to_convert)
 	#2001:db8:1000/42
