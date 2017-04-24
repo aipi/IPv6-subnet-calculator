96e5b000b492c5588d70c0886c9dac228efe5fd1
aipi <tesouraet@gmail.com>
2017-04-24 00:26:57 -0300

    first part of calculator working so well

--- subnet_calculator.py
+++ subnet_calculator.py
@@ -5,12 +5,26 @@ def Calculator(net, range):
 			range_to_convert = int(input('Type it the net range which you want to convert: '))
 		except ValueError:
 			print('Please, enter a valid net range, between 1 and 128.')
-	print(net)
+	
+	i, bit_total, group, bit_group, bit = 1, 0, 0, 0, 0
+	while(i <= 32):
+		j = 0
+		while(j < 4):
+			if(bit != range_to_convert):
+				j += 1
+				bit += 1
+			else:
+				bit_total = bit 
+				bit_group = j
+				group = i
+				break
+		if(bit_total == range_to_convert):
+			break
+		i += 1
 
+	print('The calue to be changed is int the bit {}, in the group {} and in the position {}'.format(bit_total + 1, group, bit_group))
 
-
-
-# 2001:0DB8::140B/33 
+# 2001:0DB8::140B/33
 # 2001:0DB8:0000:0000:130F:0000:0000:140B/33 -> /32
 #    REDE <-|-> HOST
 #           |
